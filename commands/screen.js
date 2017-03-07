6c8b99127488887a0d5276912952bbcbfab5440e
mwarger <mwarger@gmail.com>
2017-03-07 01:11:55 -0600

    updated screen command to use screen-style template

--- commands/screen.js
+++ commands/screen.js
@@ -1,19 +1,19 @@
 // @cliDescription  Generates an opinionated container.
 
-module.exports = async function (context) {
+module.exports = async function(context) {
   // grab some features
-  const { parameters, print, strings, ignite } = context
-  const { pascalCase, isBlank } = strings
+  const { parameters, print, strings, ignite } = context;
+  const { pascalCase, isBlank } = strings;
 
   // validation
   if (isBlank(parameters.first)) {
-    print.info(`${context.runtime.brand} generate saga <name>\n`)
-    print.info('A name is required.')
-    return
+    print.info(`${context.runtime.brand} generate saga <name>\n`);
+    print.info('A name is required.');
+    return;
   }
 
-  const name = pascalCase(parameters.first)
-  const props = { name }
+  const name = pascalCase(parameters.first);
+  const props = { name };
 
   const jobs = [
     {
@@ -21,11 +21,11 @@ module.exports = async function (context) {
       target: `App/Containers/${name}Screen.js`
     },
     {
-      template: `saga.ejs`,
+      template: `screen-style.ejs`,
       target: `App/Containers/Styles/${name}ScreenStyle.js`
     }
-  ]
+  ];
 
   // make the templates
-  await ignite.copyBatch(context, jobs, props)
-}
+  await ignite.copyBatch(context, jobs, props);
+};
