0836e818786b06d047797e5863c95d80133614ab
Gant Laborde <GantMan@gmail.com>
2017-03-22 11:56:19 -0500

    screen generator is using new nav

--- commands/screen.js
+++ commands/screen.js
@@ -2,8 +2,9 @@
 
 module.exports = async function (context) {
   // grab some features
-  const { parameters, print, strings, ignite } = context
+  const { parameters, print, strings, ignite, filesystem, patching } = context
   const { pascalCase, isBlank } = strings
+  const config = ignite.loadIgniteConfig()
 
   // validation
   if (isBlank(parameters.first)) {
@@ -28,4 +29,34 @@ module.exports = async function (context) {
 
   // make the templates
   await ignite.copyBatch(context, jobs, props)
+
+  // if using `react-navigation` go the extra step
+  // and insert the screen into the nav router
+  if (config.navigation === 'react-navigation') {
+    const screenName = `${name}Screen`
+    const appNavFilePath = `${process.cwd()}/App/Navigation/AppNavigation.js`
+    const importToAdd = `import ${screenName} from '../Containers/${screenName}'`
+    const routeToAdd = `  ${screenName}: { screen: ${screenName} },`
+
+    if (!filesystem.exists(appNavFilePath)) {
+      const msg = `No '${appNavFilePath}' file found.  Can't insert screen.`
+      print.error(msg)
+      process.exit(1)
+    }
+
+    // insert screen import
+    ignite.patchInFile(appNavFilePath, {
+      after: 'import { StackNavigator } from',
+      insert: importToAdd
+    })
+
+    // insert screen route
+    ignite.patchInFile(appNavFilePath, {
+      after: 'const PrimaryNav',
+      insert: routeToAdd
+    })
+
+  } else {
+    print.log('Screen created, manually add it to your navigation')
+  }
 }
