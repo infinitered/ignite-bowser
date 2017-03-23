e9d3fd6fa1b5041ac364f12b86096d73d5b0f85c
Gant <GantMan@gmail.com>
2017-03-23 17:08:38 -0500

    cleanup files

--- boilerplate/App/Containers/LaunchScreen.js
+++ boilerplate/App/Containers/LaunchScreen.js
@@ -19,7 +19,7 @@ export default class LaunchScreen extends React.Component {
           <View style={styles.section} >
             <Image source={Images.ready} />
             <Text style={styles.sectionText}>
-              {"This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite."}
+              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
             </Text>
           </View>
 
