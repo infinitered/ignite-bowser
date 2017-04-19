b45165a572481103c7a59208625bd24be35e4047
RWOverdijk <r.w.overdijk@gmail.com>
2017-04-19 09:10:58 +0200

    add patterns file

---
+++ commands/patterns.js
@@ -0,0 +1,10 @@
+const constants = {
+  PATTERN_IMPORTS: 'imports',
+  PATTERN_ROUTES: 'routes'
+};
+
+module.exports = {
+  constants,
+  [constants.PATTERN_IMPORTS]: `import\\s+.+?\\s+from\\s+'react-navigation';?`,
+  [constants.PATTERN_ROUTES]: 'const PrimaryNav',
+};
