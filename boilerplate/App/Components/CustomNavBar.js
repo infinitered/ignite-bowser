83c0431c3220bcad0b26a9651eeea40967090a75
Yota Toyama <raviqqe@gmail.com>
2017-05-18 14:19:30 +0900

    Fix inconsistent "styles" styles

--- boilerplate/App/Components/CustomNavBar.js
+++ boilerplate/App/Components/CustomNavBar.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { View, Image, Animated, TouchableOpacity } from 'react-native'
 import { Images, Colors } from '../Themes'
-import Styles from './Styles/CustomNavBarStyles'
+import styles from './Styles/CustomNavBarStyles'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { Actions as NavigationActions } from 'react-native-router-flux'
 
 export default class CustomNavBar extends React.Component {
   render () {
     return (
-      <Animated.View style={Styles.container}>
-        <TouchableOpacity style={Styles.leftButton} onPress={NavigationActions.pop}>
+      <Animated.View style={styles.container}>
+        <TouchableOpacity style={styles.leftButton} onPress={NavigationActions.pop}>
           <Icon name='ios-arrow-back' size={34} color={Colors.snow} />
         </TouchableOpacity>
-        <Image style={Styles.logo} source={Images.clearLogo} />
-        <View style={Styles.rightButton} />
+        <Image style={styles.logo} source={Images.clearLogo} />
+        <View style={styles.rightButton} />
       </Animated.View>
     )
   }
