f7ce42361d48c1bf1b520273c1ea87a3cd47a382
Gant Laborde <GantMan@gmail.com>
2017-08-14 15:11:27 -0500

    linting fixings

--- commands/listview.js
+++ commands/listview.js
@@ -1,14 +1,9 @@
 // @cliDescription  Deprecated - Use `list`
 
-const patterns = require('../lib/patterns')
-
 module.exports = async function (context) {
   const { print } = context
-  const config = ignite.loadIgniteConfig()
 
   print.info(`Listview is now simply 'list' and supports multiple types of views.\n`)
   print.info(`Instead run: ${context.runtime.brand} generate list <name>\n`)
   print.info('This warning will be removed in a future release.')
-
-  return
 }
