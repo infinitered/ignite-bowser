a5c2eb59095d744137d26169228786a985793fc3
libre <jbbriant44@gmail.com>
2017-09-28 16:35:38 +0200

    FT - enable HMR for reducers/sagas/containers
    See https://github.com/reactjs/react-redux/releases/tag/v2.0.0 for description

--- boilerplate/App/Containers/App.js
+++ boilerplate/App/Containers/App.js
@@ -9,6 +9,23 @@ import createStore from '../Redux'
 const store = createStore()
 
 /**
+* From Redux 2.0.0, HMR for reducers is not by default.
+* To avoid an error when we change a reducer, a saga or a container, we must explicitely reload the store
+* For unknown reason there is still an error at first reloading but then it leave and the store is updated
+* TODO: investigate on this first strange error, the HMR still works
+*
+* <Provider> does not support changing `store` on the fly. It is most likely that you see this error because 
+* you updated to Redux 2.x and React Redux 2.x which no longer hot reload reducers automatically. 
+* See https://github.com/reactjs/react-redux/releases/tag/v2.0.0 for the migration instructions.
+*/
+if (module.hot) {
+  module.hot.accept(() => {
+    const nextRootReducer = require('../Redux')
+    store.replaceReducer(nextRootReducer.default)
+  })
+}
+
+/**
  * Provides an entry point into our application.  Both index.ios.js and index.android.js
  * call this component first.
  *
