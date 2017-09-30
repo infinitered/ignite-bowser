2b8bf23be0b0c1d3d11f037e094b76494c1db73b
libre <jbbriant44@gmail.com>
2017-09-30 23:57:33 +0200

    FIX - HMR now fully works
    It took me 15 hours to figure that it was as simple than this. The looooooooonnnngest fix I've ever made.
    But as always, success ! 
    
    - HMR works as expected for components
    - reducers are editable and state is kept between reloads
    - linting should be OK this time ;)
    
    Todo:
    - something could be tried to do the same with sagas if needed -> https://stackoverflow.com/questions/37148592/redux-saga-hot-reloading
    
    <3

--- boilerplate/App/Redux/index.js
+++ boilerplate/App/Redux/index.js
@@ -2,13 +2,22 @@ import { combineReducers } from 'redux'
 import configureStore from './CreateStore'
 import rootSaga from '../Sagas/'
 
-export default () => {
-  /* ------------- Assemble The Reducers ------------- */
-  const rootReducer = combineReducers({
-    nav: require('./NavigationRedux').reducer,
-    github: require('./GithubRedux').reducer,
-    search: require('./SearchRedux').reducer
-  })
+/* ------------- Assemble The Reducers ------------- */
+export const reducers = combineReducers({
+  nav: require('./NavigationRedux').reducer,
+  github: require('./GithubRedux').reducer,
+  search: require('./SearchRedux').reducer
+})
 
-  return configureStore(rootReducer, rootSaga)
+export default () => {
+  const store = configureStore(reducers, rootSaga)
+
+  if (module.hot) {
+    module.hot.accept(() => {
+      const nextRootReducer = require('./').reducers
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+
+  return store
 }
