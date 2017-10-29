5aa4ca293f6c7cc6f132da68e781a56f8b1f81fc
Karl Metum <karlo.m@gmail.com>
2017-10-29 17:41:36 +0100

    Fix typo (#160)

--- boilerplate/App/Navigation/ReduxNavigation.js
+++ boilerplate/App/Navigation/ReduxNavigation.js
@@ -3,7 +3,7 @@ import * as ReactNavigation from 'react-navigation'
 import { connect } from 'react-redux'
 import AppNavigation from './AppNavigation'
 
-// here is our redux-aware our smart component
+// here is our redux-aware smart component
 function ReduxNavigation (props) {
   const { dispatch, nav } = props
   const navigation = ReactNavigation.addNavigationHelpers({
