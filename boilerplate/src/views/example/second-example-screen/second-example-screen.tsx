d9db3dfed3be0c88b2f4c9ead01f3e188a9470dc
Gant Laborde <gantman@gmail.com>
2018-04-25 17:09:53 +0300

    fixed image on tron display

--- boilerplate/src/views/example/second-example-screen/second-example-screen.tsx
+++ boilerplate/src/views/example/second-example-screen/second-example-screen.tsx
@@ -118,7 +118,7 @@ export class SecondExampleScreen extends React.Component<SecondExampleScreenProp
                     value: {a: 1, b: [1,2,3]},
                     preview: "when you click here, it might surprise you!",
                     important: true,
-                    image: "http://placekitten.com/g/400/400",
+                    image: { uri: "https://avatars2.githubusercontent.com/u/997157?s=460&v=4" },
                   })
                   // make an API call for the demo
                   // Don't do API like this, use store's API
