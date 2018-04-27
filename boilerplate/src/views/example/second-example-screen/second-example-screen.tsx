fbe4a8370279ed0b57a74e4b74b92b3605a7a921
Steve Kellock <steve@kellock.ca>
2018-04-27 17:55:48 -0400

    💄 Switches to the Infinite Red brand (#9)

--- boilerplate/src/views/example/second-example-screen/second-example-screen.tsx
+++ boilerplate/src/views/example/second-example-screen/second-example-screen.tsx
@@ -92,11 +92,27 @@ export class SecondExampleScreen extends React.Component<SecondExampleScreenProp
     console.tron.log("Your Friendly tron log message")
     console.tron.logImportant("I am important")
     console.tron.display({
-      name: "Witty Here",
-      value: {a: 1, b: [1,2,3]},
-      preview: "when you click here, it might surprise you!",
+      name: "DISPLAY",
+      value: {
+        numbers: 1,
+        strings: "strings",
+        booleans: true,
+        arrays: [1, 2, 3],
+        objects: {
+          deeper: {
+            deeper: {
+              yay: "👾",
+            },
+          },
+        },
+        functionNames: function hello() {},
+      },
+      preview: "More control with display()",
       important: true,
-      image: { uri: "https://avatars2.githubusercontent.com/u/997157?s=460&v=4" },
+      image: {
+        uri:
+          "https://avatars2.githubusercontent.com/u/3902527?s=200&u=a0d16b13ed719f35d95ca0f4440f5d07c32c349a&v=4",
+      },
     })
     // make an API call for the demo
     // Don't do API like this, use store's API
