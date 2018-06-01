68b6a5e7219a7714c24f54e896277bc5e6843bb7
Gant Laborde <gantman@gmail.com>
2018-06-01 14:32:25 -0500

    using react nav 2.x (#29)

--- boilerplate/src/navigation/example-navigator.ts
+++ boilerplate/src/navigation/example-navigator.ts
@@ -1,8 +1,8 @@
-import { StackNavigator } from "react-navigation"
+import { createStackNavigator } from "react-navigation"
 import { FirstExampleScreen } from "../views/example/first-example-screen"
 import { SecondExampleScreen } from "../views/example/second-example-screen"
 
-export const ExampleNavigator = StackNavigator({
+export const ExampleNavigator = createStackNavigator({
   firstExample: { screen: FirstExampleScreen },
   secondExample: { screen: SecondExampleScreen },
 },
