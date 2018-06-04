e78a899051f474da8807345665013a144f460766
Ryan Linton <ryan@infinite.red>
2018-06-05 05:57:35 +0700

    Improve how domains work and register stores with root-store

--- commands/model.js
+++ commands/model.js
@@ -3,7 +3,7 @@
 module.exports = async function (context) {
   // grab some features
   const { parameters, strings, print, ignite, filesystem, patching, prompt } = context
-  const { kebabCase, pascalCase, isBlank } = strings
+  const { camelCase, kebabCase, pascalCase, isBlank } = strings
   const options = parameters.options || {}
   const folder = options.folder || options.f
 
@@ -13,6 +13,11 @@ module.exports = async function (context) {
     print.info('A name is required.')
     return
   }
+
+  const givenName = parameters.first
+  const name = kebabCase(givenName)
+  const pascalName = pascalCase(givenName)
+  const camelName = camelCase(givenName)
 
   const domains = filesystem.list('./src/models/') || []
   const domainChoices = ['(Create New)', ...domains]
@@ -26,29 +31,41 @@ module.exports = async function (context) {
       message: domainQuestion,
       choices: domainChoices
     })
-    domainPath = (domainAddAnswer.domain === domainChoices[0]) ? '' : domainAddAnswer.domain + '/'
+    domainPath = (domainAddAnswer.domain === domainChoices[0]) ? name : domainAddAnswer.domain
   } else {
-    domainPath = (folder === 'models') ? '' : folder + '/'
+    domainPath = (folder === 'models') ? '' : folder
   }
-
-  const givenName = parameters.first
-  const name = kebabCase(givenName)
-  const pascalName = pascalCase(givenName)
-  // const newDomain = isBlank(domainPath)
 
   const props = { name, pascalName }
   const jobs = [
     {
       template: 'model.ejs',
-      target: `src/models/${domainPath}${name}/${name}.ts`
+      target: `src/models/${domainPath}/${name}.ts`
     }, {
       template: 'model.test.ejs',
-      target: `src/models/${domainPath}${name}/${name}.test.ts`
-    }, {
-      template: 'rollup-index.ts.ejs',
-      target: `src/models/${domainPath}${name}/index.ts`
+      target: `src/models/${domainPath}/${name}.test.ts`
     }
   ]
 
+  const rollupPath = `src/models/${domainPath}/index.ts`
+  const newDomain = domainPath === name
+
+  if (newDomain) {
+    jobs.push({ template: 'rollup-index.ts.ejs', target: rollupPath })
+  } else {
+    patching.insertInFile(rollupPath, 'export', `export * from "./${name}"`, false)
+  }
+
   await ignite.copyBatch(context, jobs, props)
+
+  // include stores in root-store
+  if (name.endsWith('store')) {
+    const rootStorePath = './src/app/root-store.ts'
+    const rootStoreDef = 'export const RootStoreModel'
+    const storeTypeImport = `import { ${pascalName}Model } from "../models/${name}"`
+    const storeType = `  ${camelName}: types.optional(${pascalName}Model, {}),`
+
+    patching.insertInFile(rootStorePath, 'import', storeTypeImport)
+    patching.insertInFile(rootStorePath, rootStoreDef, storeType)
+  }
 }
