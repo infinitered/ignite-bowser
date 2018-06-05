3cb128b3f037e3a3e8034c1d4c53916875880896
Leon Kim <minusnine@gmail.com>
2018-06-05 21:42:04 +0900

    Fix: hot module must be in a class for HMR

--- boilerplate/storybook/storybook.ts
+++
@@ -1,7 +0,0 @@
-import { getStorybookUI, configure } from "@storybook/react-native"
-
-configure(() => {
-  require("./storybook-registry")
-})
-
-export const StorybookUI = getStorybookUI({ port: 9001, host: "localhost", onDeviceUI: true })
