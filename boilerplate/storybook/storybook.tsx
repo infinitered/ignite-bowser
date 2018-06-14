e5d5414ea3f957c49429575cf1d44fd3cf413395
Leon S. Kim <minusnine@gmail.com>
2018-06-14 15:44:46 +0900

    Fix: hide splash screen when running storybook (#53)

--- boilerplate/storybook/storybook.tsx
+++ boilerplate/storybook/storybook.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getStorybookUI, configure } from "@storybook/react-native"
+import SplashScreen from 'react-native-splash-screen'
 
 configure(() => {
   require("./storybook-registry")
@@ -9,6 +10,9 @@ const StorybookUI = getStorybookUI({ port: 9001, host: "localhost", onDeviceUI: 
 
 // RN hot module must be in a class for HMR
 export class StorybookUIRoot extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide()
+  }
   render() {
     return <StorybookUI />
   }
