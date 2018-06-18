f79d6d386b3501b7f10bba0e5a612ae8a0a3deb7
Leon Kim <minusnine@gmail.com>
2018-06-19 04:54:49 +0900

    Update: move react-native-splash-screen to package.json.ejs

--- boilerplate.js
+++ boilerplate.js
@@ -158,10 +158,6 @@ async function install (context) {
   async function patchSplashScreen () {
     spinner.text = `▸ setting up splash screen`
     spinner.start()
-    spinner.text = `▸ setting up splash screen: installing package`
-    await system.run(`yarn add react-native-splash-screen@3.0.6`, { stdio: 'ignore' })
-    spinner.text = `▸ setting up splash screen: linking`
-    await system.spawn(`react-native link react-native-splash-screen`, { stdio: 'ignore' })
     spinner.text = `▸ setting up splash screen: configuring`
     const backupExtension = (os.platform() === 'darwin') ? '""' : ''
     await system.run(`sed -i ${backupExtension} 's/SplashScreenPatch/${name}/g' ${process.cwd()}/patches/splash-screen/splash-screen.patch`, { stdio: 'ignore' })
