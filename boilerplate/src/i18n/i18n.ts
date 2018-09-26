d0dec1633f9755712fa2c8cb6c0415f3f9ad4bbe
Marcel Kalveram <marcel.kalveram@googlemail.com>
2018-09-26 10:52:32 +0200

    fix linting errors

--- boilerplate/src/i18n/i18n.ts
+++ boilerplate/src/i18n/i18n.ts
@@ -1,9 +1,9 @@
-import RNLanguages from 'react-native-languages';
-import i18n from 'i18n-js';
+import RNLanguages from "react-native-languages"
+import i18n from "i18n-js"
 
 const en = require("./en")
 const ja = require("./ja")
 
-i18n.locale = RNLanguages.language;
+i18n.locale = RNLanguages.language
 i18n.fallbacks = true
 i18n.translations = { en, ja }
