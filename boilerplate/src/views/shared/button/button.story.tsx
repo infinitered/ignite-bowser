2566c0cc62b80fbb7d4044e915bbd4b8fe1da381
Morgan Laco <morganlaco@gmail.com>
2018-09-27 10:47:26 -0400

    Restore preset

--- boilerplate/src/views/shared/button/button.story.tsx
+++ boilerplate/src/views/shared/button/button.story.tsx
@@ -14,7 +14,7 @@ storiesOf("Button", module)
   .add("Style Presets", () => (
     <Story>
       <UseCase text="Primary" usage="The primary button.">
-        <Button text="Click It" onPress={() => window.alert("pressed")} />
+        <Button text="Click It" preset="primary" onPress={() => window.alert("pressed")} />
       </UseCase>
       <UseCase text="Disabled" usage="The disabled behaviour of the primary button.">
         <Button text="Click It" preset="primary" onPress={() => window.alert("pressed")} disabled />
