11a0e3bae182e508dbc3225ab4871784d863b0d2
Robin Heinze <robin.m.heinze@gmail.com>
2019-01-25 17:02:41 -0800

    Add link to Detox docs in spec file

--- boilerplate/e2e/firstTest.spec.js
+++ boilerplate/e2e/firstTest.spec.js
@@ -1,3 +1,6 @@
+// For more info on how to write Detox tests, see the official docs:
+// https://github.com/wix/Detox/blob/master/docs/README.md
+
 describe('Example', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
