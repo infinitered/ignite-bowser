0790791320e52b4a8d1da312510b5d611baaba71
Addin Gama Bertaqwa <punyagama@gmail.com>
2019-03-18 06:55:39 +0800

    Fix double slash typo on setup-root-store.ts

--- boilerplate/app/models/root-store/setup-root-store.ts
+++ boilerplate/app/models/root-store/setup-root-store.ts
@@ -1,7 +1,7 @@
 import { onSnapshot } from "mobx-state-tree"
 import { RootStoreModel, RootStore } from "./root-store"
 import { Environment } from "../environment"
-import * as storage from "../..//utils/storage"
+import * as storage from "../../utils/storage"
 import { Reactotron } from "../../services/reactotron"
 import { Api } from "../../services/api"
 
