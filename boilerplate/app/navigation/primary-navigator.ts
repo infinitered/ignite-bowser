e3b7f68515269944a8b7b271a03227a20274376f
Bryan Stearns <bryanstearns@gmail.com>
2019-08-04 12:17:47 -0700

    Move exit route list to primaryNavigator

--- boilerplate/app/navigation/primary-navigator.ts
+++ boilerplate/app/navigation/primary-navigator.ts
@@ -11,3 +11,12 @@ export const PrimaryNavigator = createStackNavigator(
     headerMode: "none",
   },
 )
+
+/**
+ * A list of routes from which we're allowed to leave the app when
+ * the user presses the back button on Android.
+ *
+ * Anything not on this list will be a standard `back` action in
+ * react-navigation.
+ */
+export const exitRoutes: string[] = ["welcome"]
