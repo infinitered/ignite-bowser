3f7894cbd37dd8bc5437515a73ef4275da22ad2e
Jamon Holmgren <jamonholmgren@gmail.com>
2019-10-07 15:47:48 -0700

    fix(boilerplate): Update to React Native 0.61 (#263 by @jamonholmgren)

--- boilerplate/app/components/icon/icons/index.ts
+++ boilerplate/app/components/icon/icons/index.ts
@@ -1,6 +1,6 @@
 export const icons = {
-  back: require("./arrow-left.png"),
-  bullet: require("./bullet.png"),
+  back: require("./arrow-left@2x.png"),
+  bullet: require("./bullet@2x.png"),
 }
 
 export type IconTypes = keyof typeof icons
