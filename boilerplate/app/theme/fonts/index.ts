7cf7854a68bbcc40782695c7e927959deeba234e
Leon Kim <minusnine@gmail.com>
2019-12-11 07:11:30 +0900

    remove: unnecessary promise

--- boilerplate/app/theme/fonts/index.ts
+++ boilerplate/app/theme/fonts/index.ts
@@ -1,10 +1,8 @@
 import * as Font from "expo-font"
 
 export const initFonts = async () => {
-  await Promise.all([
-    Font.loadAsync({
-      "Montserrat": require("./Montserrat-Regular.ttf"),
-      "Montserrat-Regular": require("./Montserrat-Regular.ttf"),
-    }),
-  ])
+  await Font.loadAsync({
+    "Montserrat": require("./Montserrat-Regular.ttf"),
+    "Montserrat-Regular": require("./Montserrat-Regular.ttf"),
+  })
 }
