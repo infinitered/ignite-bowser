a9526037b29c216c943715b62bf4303ca40ec40a
Jamon Holmgren <jamon@infinite.red>
2019-12-23 12:39:46 -0800

    Switch to using NPX for ignite CLI commands

--- test/generators-integration.test.ts
+++ test/generators-integration.test.ts
@@ -2,32 +2,28 @@ const execa = require("execa")
 const jetpack = require("fs-jetpack")
 const tempy = require("tempy")
 
-const IGNITE = "ignite"
+const IGNITE_COMMAND = "npx ignite-cli"
 const APP = "IntegrationTest"
 const BOILERPLATE = `${__dirname}/../`
+
 // calling the ignite cli takes a while
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 800000
 
-const execaShell = (command, opts = {}) => execa(command, { shell: true, ...opts })
+const execaShell = (command: string, opts = {}) => execa(command, { shell: true, ...opts })
 
 describe("a generated app", () => {
   // creates a new temp directory
-  const appTemp = tempy.directory()
+  const appTemp: string = tempy.directory()
   beforeAll(async () => {
     // make sure we are in the temp directory. Do the initial git commit
     // manually, so we can set up the git user first on circleci.
     process.chdir(appTemp)
-    await execa(IGNITE, [
-      "new",
-      APP,
-      "--no-detox",
-      "--skip-git",
-      "--debug",
-      "--overwrite",
-      "--boilerplate",
-      BOILERPLATE,
-    ])
+
+    const flags = ["--no-detox", "--no-expo", "--skip-git", "--debug", "--overwrite"].join(" ")
+    await execaShell(`${IGNITE_COMMAND} new ${APP} ${flags} --boilerplate ${BOILERPLATE}`)
+
     process.chdir(APP)
+
     await execaShell("git init")
     await execaShell('git config user.email "test@example.com"')
     await execaShell("git config user.name test")
@@ -40,7 +36,7 @@ describe("a generated app", () => {
     jetpack.remove(appTemp)
   })
 
-  test("can yarn install and pass tests", () => {
+  test.only("can yarn install and pass tests", () => {
     return expect(
       execaShell("yarn test 2>&1")
         .then(() => execaShell("git status --porcelain"))
@@ -60,7 +56,7 @@ describe("a generated app", () => {
 
   test("generates a component", async () => {
     const simpleComponent = "Simple"
-    await execa(IGNITE, ["g", "component", simpleComponent], { preferLocal: false })
+    await execa(IGNITE_COMMAND, ["g", "component", simpleComponent], { preferLocal: false })
     expect(jetpack.exists(`app/components/${simpleComponent}/${simpleComponent}.tsx`)).toBe("file")
     expect(jetpack.exists(`app/components/${simpleComponent}/${simpleComponent}.story.tsx`)).toBe(
       "file",
@@ -71,7 +67,7 @@ describe("a generated app", () => {
 
   test("generates a screen", async () => {
     const simpleScreen = "test"
-    await execa(IGNITE, ["g", "screen", simpleScreen], { preferLocal: false })
+    await execa(IGNITE_COMMAND, ["g", "screen", simpleScreen], { preferLocal: false })
     expect(jetpack.exists(`app/screens/${simpleScreen}-screen.tsx`)).toBe("file")
     const lint = await execa("npm", ["-s", "run", "lint"])
     expect(lint.stderr).toBe("")
@@ -79,7 +75,7 @@ describe("a generated app", () => {
 
   test("generates a model", async () => {
     const simpleModel = "test"
-    await execa(IGNITE, ["g", "model", simpleModel], { preferLocal: false })
+    await execa(IGNITE_COMMAND, ["g", "model", simpleModel], { preferLocal: false })
     expect(jetpack.exists(`app/models/${simpleModel}/${simpleModel}.ts`)).toBe("file")
     expect(jetpack.exists(`app/models/${simpleModel}/${simpleModel}.test.ts`)).toBe("file")
     expect(jetpack.exists(`app/models/${simpleModel}/index.ts`)).toBe("file")
@@ -90,7 +86,7 @@ describe("a generated app", () => {
   test("generates navigation", async () => {
     const simpleNavigation = "test"
     await execa(
-      IGNITE,
+      IGNITE_COMMAND,
       [
         "g",
         "navigator",
