3a23cccde6c7069e7946313e6a6dcbc76429b169
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2020-01-01 21:34:46 +0100

    Added parameters for choosing component type. Updated style reference to be camel case.

--- src/commands/generate/component.ts
+++ src/commands/generate/component.ts
@@ -4,7 +4,7 @@ export const description = "Generates a component, supporting files, and a story
 export const run = async function(toolbox: GluegunToolbox) {
   // grab some features
   const { parameters, strings, print, ignite, patching, filesystem, prompt } = toolbox
-  const { pascalCase, isBlank } = strings
+  const { pascalCase, camelCase, isBlank } = strings
 
   // validation
   if (isBlank(parameters.first)) {
@@ -13,29 +13,34 @@ export const run = async function(toolbox: GluegunToolbox) {
     return
   }
 
-  const componentTypes = [
-    {
-      name: 'functionComponent',
-      message: 'React.FunctionComponent, aka "hooks component"'
-    },
-    {
-      name: 'statelessFunction',
-      message: 'Stateless function, aka the "classic" ignite-bowser component'
-    }
-  ]
+  let componentType
+  if (!parameters.options["function-component"] && !parameters.options["stateless-function"]) {
+    const componentTypes = [
+      {
+        name: "functionComponent",
+        message: "React.FunctionComponent, aka \"hooks component\"",
+      },
+      {
+        name: "statelessFunction",
+        message: "Stateless function, aka the \"classic\" ignite-bowser component",
+      },
+    ]
 
-  const { componentType } = await prompt.ask([
-    {
-      name: 'componentType',
-      message: 'Which type of component do you want to generate?',
-      type: 'select',
-      choices: componentTypes,
-    },
-  ])
+    const { component } = await prompt.ask([
+      {
+        name: "component",
+        message: "Which type of component do you want to generate?",
+        type: "select",
+        choices: componentTypes,
+      },
+    ])
+    componentType = component
+  }
 
   const name = parameters.first
   const pascalName = pascalCase(name)
-  const props = { name, pascalName }
+  const camelCaseName = camelCase(name)
+  const props = { name, pascalName, camelCaseName }
 
   const jobs = [
     {
@@ -48,14 +53,14 @@ export const run = async function(toolbox: GluegunToolbox) {
     }
   ]
 
-  if (componentType === "functionComponent") {
+  if (componentType === "functionComponent" || parameters.options["function-component"]) {
     jobs.push(
       {
         template: 'function-component.tsx.ejs',
         target: `app/components/${name}/${name}.tsx`
       }
     )
-  } else if (componentType === "statelessFunction") {
+  } else if (componentType === "statelessFunction" || parameters.options["stateless-function"]) {
     jobs.push(
       {
         template: 'component.tsx.ejs',
