19617d46dbbcf13be0d93b7fb9fcf7d33acd2b6d
Leon Kim <minusnine@gmail.com>
2020-01-07 05:40:03 +0900

    chore: update react-navigation

--- boilerplate/app/navigation/root-navigator.ts
+++ boilerplate/app/navigation/root-navigator.ts
@@ -1,4 +1,4 @@
-import { createStackNavigator } from "react-navigation"
+import { createStackNavigator } from "react-navigation-stack"
 import { PrimaryNavigator } from "./primary-navigator"
 import {
 } from "../screens" // eslint-disable-line @typescript-eslint/no-unused-vars
