12732c4aa8a33ecc99a2d221d9168c532bcb3e8e
Bryan Stearns <bryanstearns@gmail.com>
2020-03-24 13:55:44 -0700

    Generate & test an Expo app too; break up the main test into individual tests
    - Added a utility function, expectCommandToSucceed, that enables nice simple individual tests
    - Added an afterEach to clean up generated files between tests
    - Added --verbose to the test command; seeing individual test progress was useful in debugging
    - Removed the test for a linting script; since expo & non-expo use different commands now, didn't seem worth the complication.

--- test/generators-integration.test.ts
+++ test/generators-integration.test.ts
@@ -11,112 +11,120 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 800000
 
 const execaShell = (command: string, opts = {}) => execa(command, { shell: true, ...opts })
 
-describe("a generated app", () => {
-  // creates a new temp directory
-  const appTemp: string = tempy.directory()
-  beforeAll(async () => {
-    // make sure we are in the temp directory. Do the initial git commit
-    // manually, so we can set up the git user first on circleci.
-    process.chdir(appTemp)
+// Run a shell command; if it fails, expect that its output was empty, so that jest
+// shows us the error output. Return the result so we can assert further on it.
+const expectCommandToSucceed = async (command: string) => {
+  const result = await execaShell(command + " 2>&1").catch(error => error)
+  if (result.exitCode !== 0) {
+    expect(result).toMatchObject({ stdout: "" })
+  }
+  return result
+}
 
-    const flags = ["--no-detox", "--no-expo", "--skip-git", "--debug", "--overwrite"].join(" ")
-    await execaShell(`${IGNITE_COMMAND} new ${APP} ${flags} --boilerplate ${BOILERPLATE}`)
+// Do these tests for non-expo and expo apps
+const expoFlags = ["--no-expo", "--expo"]
+expoFlags.forEach(expoFlag => {
+  describe(`a generated ${expoFlag === "--expo" ? "Expo" : "non-Expo"} app`, () => {
+    // creates a new temp directory
+    const appTemp: string = tempy.directory()
+    beforeAll(async () => {
+      // make sure we are in the temp directory. Do the initial git commit
+      // manually, so we can set up the git user first on circleci.
+      process.chdir(appTemp)
 
-    process.chdir(APP)
+      const flags = ["--no-detox", expoFlag, "--skip-git", "--debug", "--overwrite"].join(" ")
+      await execaShell(`${IGNITE_COMMAND} new ${APP} ${flags} --boilerplate ${BOILERPLATE}`)
 
-    await execaShell("git init")
-    await execaShell('git config user.email "test@example.com"')
-    await execaShell("git config user.name test")
-    await execaShell("git add -A")
-    await execaShell('git commit -m "Initial commit"')
-  })
+      process.chdir(APP)
 
-  afterAll(() => {
-    // clean up generated test app
-    jetpack.remove(appTemp)
-  })
-
-  test("can yarn install and pass tests", () => {
-    return expect(
-      execaShell("yarn test 2>&1")
-        .then(() => execaShell("git status --porcelain"))
-        .then(({ stdout }) => expect(stdout).toEqual(""))
-        .then(() =>
-          execaShell("yarn compile 2>&1 && yarn format 2>&1 && yarn lint --max-warnings 0 2>&1"),
-        )
-        .then(() => execaShell("git status --porcelain"))
-        .catch(error => error),
-    ).resolves.toMatchObject({ stdout: "" }) // will fail & show the yarn or test errors
-  })
-
-  test("does have a linting script", () => {
-    expect(jetpack.read("package.json", "json").scripts.lint).toBe(
-      "eslint index.js app storybook test --fix --ext .js,.ts,.tsx",
-    )
-  })
-
-  test("generates a stateless function", async () => {
-    const statelessFunction = "Stateless"
-    await execaShell(`${IGNITE_COMMAND} g component ${statelessFunction} --stateless-function`, {
-      preferLocal: false,
+      await execaShell("git init")
+      await execaShell('git config user.email "test@example.com"')
+      await execaShell("git config user.name test")
+      await execaShell("git add -A")
+      await execaShell('git commit -m "Initial commit"')
     })
-    expect(jetpack.exists(`app/components/${statelessFunction}/${statelessFunction}.tsx`)).toBe(
-      "file",
-    )
-    expect(
-      jetpack.exists(`app/components/${statelessFunction}/${statelessFunction}.story.tsx`),
-    ).toBe("file")
-    expect(
-      jetpack.exists(`app/components/${statelessFunction}/${statelessFunction}.styles.ts`),
-    ).toBe("file")
-    const lint = await execa("npm", ["-s", "run", "lint"])
-    expect(lint.stderr).toBe("")
-  })
 
-  test("generates a function component", async () => {
-    const functionComponent = "FunctionComponent"
-    await execaShell(`${IGNITE_COMMAND} g component ${functionComponent} --function-component`, {
-      preferLocal: false,
+    // clean up generated files between tests; clean up the generated test app when we're done
+    afterEach(() => execaShell("git clean -fd; git reset --hard HEAD"))
+    afterAll(() => jetpack.remove(appTemp))
+
+    test("passes tests", () => {
+      return expectCommandToSucceed("yarn test")
     })
-    expect(jetpack.exists(`app/components/${functionComponent}/${functionComponent}.tsx`)).toBe(
-      "file",
-    )
-    expect(
-      jetpack.exists(`app/components/${functionComponent}/${functionComponent}.story.tsx`),
-    ).toBe("file")
-    expect(
-      jetpack.exists(`app/components/${functionComponent}/${functionComponent}.styles.ts`),
-    ).toBe("file")
-    const lint = await execa("npm", ["-s", "run", "lint"])
-    expect(lint.stderr).toBe("")
-  })
 
-  test("generates a screen", async () => {
-    const simpleScreen = "test"
-    await execaShell(`${IGNITE_COMMAND} g screen ${simpleScreen}`, { preferLocal: false })
-    expect(jetpack.exists(`app/screens/${simpleScreen}-screen.tsx`)).toBe("file")
-    const lint = await execa("npm", ["-s", "run", "lint"])
-    expect(lint.stderr).toBe("")
-  })
+    test("compiles successfully", () => {
+      return expectCommandToSucceed("yarn compile")
+    })
 
-  test("generates a model", async () => {
-    const simpleModel = "test"
-    await execaShell(`${IGNITE_COMMAND} g model ${simpleModel}`, { preferLocal: false })
-    expect(jetpack.exists(`app/models/${simpleModel}/${simpleModel}.ts`)).toBe("file")
-    expect(jetpack.exists(`app/models/${simpleModel}/${simpleModel}.test.ts`)).toBe("file")
-    expect(jetpack.exists(`app/models/${simpleModel}/index.ts`)).toBe("file")
-    const lint = await execa("npm", ["-s", "run", "lint"])
-    expect(lint.stderr).toBe("")
-  })
+    test("lints successfully", () => {
+      return expectCommandToSucceed("yarn lint --max-warnings 0")
+    })
 
-  test("generates navigation", async () => {
-    const simpleNavigation = "test"
-    await execaShell(
-      `${IGNITE_COMMAND} g navigator test --type Stack --screens DemoScreen,WelcomeScreen --navigators PrimaryNavigator`,
-      { preferLocal: false },
-    )
-    expect(jetpack.exists(`app/navigation/${simpleNavigation}-navigator.ts`)).toBe("file")
-    const lint = await execa("npm", ["-s", "run", "lint"])
-    expect(lint.stderr).toBe("")
+    test("doesn't need reformatting", async () => {
+      await expectCommandToSucceed("yarn format")
+      // Make sure format didn't change anything
+      const { stdout } = await expectCommandToSucceed("git status --porcelain")
+      expect(stdout).toEqual("")
+    })
+
+    test("generates a stateless function", async () => {
+      const statelessFunction = "Stateless"
+      await execaShell(`${IGNITE_COMMAND} g component ${statelessFunction} --stateless-function`, {
+        preferLocal: false,
+      })
+      expect(jetpack.exists(`app/components/${statelessFunction}/${statelessFunction}.tsx`)).toBe(
+        "file",
+      )
+      expect(
+        jetpack.exists(`app/components/${statelessFunction}/${statelessFunction}.story.tsx`),
+      ).toBe("file")
+      expect(
+        jetpack.exists(`app/components/${statelessFunction}/${statelessFunction}.styles.ts`),
+      ).toBe("file")
+      await expectCommandToSucceed("yarn lint")
+    })
+
+    test("generates a function component", async () => {
+      const functionComponent = "FunctionComponent"
+      await execaShell(`${IGNITE_COMMAND} g component ${functionComponent} --function-component`, {
+        preferLocal: false,
+      })
+      expect(jetpack.exists(`app/components/${functionComponent}/${functionComponent}.tsx`)).toBe(
+        "file",
+      )
+      expect(
+        jetpack.exists(`app/components/${functionComponent}/${functionComponent}.story.tsx`),
+      ).toBe("file")
+      expect(
+        jetpack.exists(`app/components/${functionComponent}/${functionComponent}.styles.ts`),
+      ).toBe("file")
+      await expectCommandToSucceed("yarn lint")
+    })
+
+    test("generates a screen", async () => {
+      const simpleScreen = "test"
+      await execaShell(`${IGNITE_COMMAND} g screen ${simpleScreen}`, { preferLocal: false })
+      expect(jetpack.exists(`app/screens/${simpleScreen}-screen.tsx`)).toBe("file")
+      await expectCommandToSucceed("yarn lint")
+    })
+
+    test("generates a model", async () => {
+      const simpleModel = "test"
+      await execaShell(`${IGNITE_COMMAND} g model ${simpleModel}`, { preferLocal: false })
+      expect(jetpack.exists(`app/models/${simpleModel}/${simpleModel}.ts`)).toBe("file")
+      expect(jetpack.exists(`app/models/${simpleModel}/${simpleModel}.test.ts`)).toBe("file")
+      expect(jetpack.exists(`app/models/${simpleModel}/index.ts`)).toBe("file")
+      await expectCommandToSucceed("yarn lint")
+    })
+
+    test("generates navigation", async () => {
+      const simpleNavigation = "test"
+      await execaShell(
+        `${IGNITE_COMMAND} g navigator test --type Stack --screens DemoScreen,WelcomeScreen --navigators PrimaryNavigator`,
+        { preferLocal: false },
+      )
+      expect(jetpack.exists(`app/navigation/${simpleNavigation}-navigator.ts`)).toBe("file")
+      await expectCommandToSucceed("yarn lint")
+    })
   })
 })
