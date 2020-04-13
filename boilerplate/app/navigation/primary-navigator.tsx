044f661e7d699be4ae80e72f6fcdeda85396f6c4
Harris Robin Kalash <hello@harris.dev>
2020-04-13 14:12:39 -0400

    fix(navigation): Use createNativeStackNavigator from react-native-screens  (#330 by @harrisrobin)

--- boilerplate/app/navigation/primary-navigator.tsx
+++ boilerplate/app/navigation/primary-navigator.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { createNativeStackNavigator } from "react-native-screens/native-stack"
 import { WelcomeScreen, DemoScreen } from "../screens"
 import { PrimaryParamList } from "./types"
 
