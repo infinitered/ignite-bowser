ab3e846da0c985c4bedb0ce33d6129dc3d598b8d
Mark Rickert <mjar81@gmail.com>
2020-05-18 20:40:54 -0400

    Removes the custom font Montserrat in favor of docs.
    No real change on Expo, just some documentation.

--- boilerplate/app/theme/fonts/index.ts
+++ boilerplate/app/theme/fonts/index.ts
@@ -1,8 +1,9 @@
 import * as Font from "expo-font"
 
 export const initFonts = async () => {
-  await Font.loadAsync({
-    Montserrat: require("./Montserrat-Regular.ttf"),
-    "Montserrat-Regular": require("./Montserrat-Regular.ttf"),
-  })
+  // Refer to ./assets/fonts/custom-fonts.md for instructions.
+  // await Font.loadAsync({
+  //   Montserrat: require("./Montserrat-Regular.ttf"),
+  //   "Montserrat-Regular": require("./Montserrat-Regular.ttf"),
+  // })
 }
