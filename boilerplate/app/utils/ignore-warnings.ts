d69df2cc6678cf4daceef9e2ac3f74883de43e73
Bryan Stearns <bryanstearns@gmail.com>
2020-08-10 17:16:19 -0700

    Update to new LogBox ignoring API; don't bother ignoring class-lifecycle warnings anymore

--- boilerplate/app/utils/ignore-warnings.ts
+++ boilerplate/app/utils/ignore-warnings.ts
@@ -2,10 +2,9 @@
  * Ignore some yellowbox warnings. Some of these are for deprecated functions
  * that we haven't gotten around to replacing yet.
  */
-import { YellowBox } from "react-native"
+import { LogBox } from "react-native"
 
-YellowBox.ignoreWarnings([
-  "componentWillMount is deprecated",
-  "componentWillReceiveProps is deprecated",
+// prettier-ignore
+LogBox.ignoreLogs([
   "Require cycle:",
 ])
