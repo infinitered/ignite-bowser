544c36ace5b32a1ce86bd94c2562b75a808efcd8
Jamon Holmgren <jamonholmgren@gmail.com>
2020-09-11 17:26:37 -0700

    Remove generators temporarily until we figure out a better way

--- src/commands/generate/navigator.ts
+++
@@ -1,8 +0,0 @@
-import { GluegunToolbox } from "gluegun"
-
-export const description = "Generates a React Navigation navigator."
-export const run = async function(toolbox: GluegunToolbox) {
-  // grab some features
-  const { print } = toolbox
-  print.warning("As of version 5, we no longer support navigator generators.")
-}
