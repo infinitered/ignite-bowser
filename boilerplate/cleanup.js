ce6db9bd442b4d7ff2eba2394019f11ec9434f29
Jamon Holmgren <jamonholmgren@gmail.com>
2020-09-12 00:03:36 -0700

    chdir

--- boilerplate/cleanup.js
+++ boilerplate/cleanup.js
@@ -5,12 +5,18 @@
 
 module.exports = {
   cleanup: async (toolbox, props) => {
-    const { print, filesystem } = toolbox
+    const { print, filesystem, system, ignite } = toolbox
 
-    if (props.boilerplate.name.toLowerCase().includes("expo")) {
-      print.info("Adding Expo packages...")
+    if (props.boilerplate.nickname === "bowser-expo") {
       // Additional setup for Expo
-      await toolbox.ignite.packager.add("expo")
+      print.info("Adding Expo packages...")
+
+      const cwd = filesystem.cwd()
+      process.chdir(__dirname)
+
+      await ignite.packager.add("expo")
+
+      process.chdir(cwd)
     }
 
     print.info(props)
